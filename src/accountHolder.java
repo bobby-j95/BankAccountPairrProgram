9a6dac1563e1ada1fdb0995a9c204257ff47b175
behulum <behulummd2018@gmail.com>
2020-10-03 13:01:17 -0400

    Behulum's updated program

--- src/accountHolder.java
+++ src/accountHolder.java
@@ -1,4 +1,13 @@
 
 public class accountHolder {
-
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String ssn;
+	private double checkingAccountOpeningBalance;
+	private double savingsAccountOpeningBalance;
+	public void AccountHolder() {
+		
+	}
+	
 }
