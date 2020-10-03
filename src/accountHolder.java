0e424d9eca911fed7a9e31f0fbc0a3ed77783be7
Robert Johns <robertjohns08@gmail.com>
2020-10-03 13:25:58 -0400

    Robert's updated code part 2

--- src/accountHolder.java
+++ src/accountHolder.java
@@ -1,13 +1,29 @@
 
 public class accountHolder {
+	
+	/*Default constructor
+	 * Created by Behulum W
+	 */
+	public void AccountHolder() {
+		
+	}
+	/*Constructor with initial values
+	 * Created by Robert J
+	 */
+	public void AccountHolder(String firstName, String middleName, String lastName, String ssn, 
+			double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.ssn = ssn;
+		this.checkingAccountOpeningBalance = checkingAccountOpeningBalance;
+		this.savingsAccountOpeningBalance = savingsAccountOpeningBalance;
+	}
+	
 	private String firstName;
 	private String middleName;
 	private String lastName;
 	private String ssn;
 	private double checkingAccountOpeningBalance;
 	private double savingsAccountOpeningBalance;
-	public void AccountHolder() {
-		
-	}
-	
 }
