c64bb767cdef01461063d305d74156b817380b9d
behulum <behulummd2018@gmail.com>
2020-10-03 20:47:43 -0400

    behulum update 5

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,4 +1,22 @@
 
 public class CheckingAccount {
 
+	/*Default constructor
+	 * Created by Behulum W
+	 */
+public void CheckingAccount() {
+		
+	}
+	/*Constructor with initial values
+	 * Created by behulum w
+	 */
+	private String CheckingAccount(double openingBalance;)
+	private double getBalance();
+	private	double getInterestRate();
+	private boolean withdraw(double amount);
+	private boolean deposit(double amount);
+	private boolean deposit(double amount);
+	private double futureValue(int years);
+	private String toString();
+
 }
