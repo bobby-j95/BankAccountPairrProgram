4947efb01933743cbdcee16f52e9131ea4c7e7a1
behulum <behulummd2018@gmail.com>
2020-10-05 12:14:04 -0400

    first commit

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,46 +1,55 @@
 public class CheckingAccount {
+	//*BEHULUM W
 
+   private double cBalance;
+
+	private static final double INTEREST_RATE = 0.01/100;
 	
-
-	private double cBalance;
-
-	private double interestRate;
-
 	public int years;
 
-	
 
 	public CheckingAccount (double openingBalance) {
 
-		this.cBalance = openingBalance;
-
-		this.interestRate = 0.01/100;
-
-		this.years = 3;
-
-		
-
+		cBalance = openingBalance;
+	
 	}
-
 	public double getBalance() {
 
-		return this.getBalance();
-
-
+		return getBalance();
 
 	}
 
 	public double getInterestRate() {
 
-		return this.interestRate;
-
-		
+		return INTEREST_RATE;
 
 	}
 
 	public int years() {
 
-		return this.years;
+		return years;
 
 	}
+		
+	public boolean withdraw(double amount) {
+		return ((amount <= cBalance) && (amount >= 0));
+	}
+	
+	public boolean deposit(double amount) {
+		return (amount > 0);
+	}
+	
+	public double futureValue(int years) {
+		return (cBalance*(Math.pow((1+INTEREST_RATE),years)));
+		
+	}
+	//* behulum w
+	public String toStrings() {
+		return("Checking Account Balance: $" + getBalance()+ "\n" + 
+				"Checking Account Interest Rate: " + getInterestRate() + "\n" +
+				"Checking Account Balance in 3 years: $" + futureValue(3));
+		
+	}
+	
 }
+
